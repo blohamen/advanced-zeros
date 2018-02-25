3569a4c488ae8184bef58f2458c2bb12884aa25d
blohamen <staver82@gmail.com>
2018-02-25 23:21:58 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
+  var divBase = {},
+      divNumber = {},
+      tmp = 0;
+primeNum(base, divBase);
+
+for(var key in divBase)
+{
+  divNumber[key] = 0;
+  for(var i = 1; i < number; i++)
+  {
+    if((tmp = Math.floor(number/ Math.pow(+key, i))) !==0){
+      divNumber[key]+= tmp;
+    } else break;
+  }
+}
+var result = [];
+for(var key in divBase)
+{
+result.push(Math.floor(divNumber[key]/divBase[key]));
+}
+return Math.min.apply(null,result);
+
+}
   // your implementation
+function primeNum(base, objBase = {})
+{
+ for(var i = 2; i<= base; i++)
+ {
+   while(base % i === 0){
+    objBase[i] ? objBase[i]++ : objBase[i] = 1;
+    base/= i;
+   }
+ }
 }
